168107619be6ee729762aac92aabe5575d50ac04
mvisani <marco.visani@unifr.ch>
2022-09-28 13:22:08 +0200

    changed CPU and RAM requierments

--- scripts/020_flye_assembly.sh
+++ scripts/020_flye_assembly.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=4G
-#SBATCH --time=06:00:00
+#SBATCH --time=20:00:00
 #SBATCH --job-name=flye_assembly
 #SBATCH --mail-user=marco.visani@students.unibe.ch
 #SBATCH --mail-type=end,error
@@ -15,5 +15,7 @@ module load UHTS/Assembler/flye/2.8.3;
 FILES=/data/users/mvisani/genome_assembly/participant_2/pacbio/*
 OUTDIR=/data/users/mvisani/genome_assembly/assembly/pacbio_flye/
 
-flye --pacbio-raw $FILES \
---out-dir $OUTDIR
+flye \
+    --pacbio-raw $FILES \
+    --genome-size 130m \
+    --out-dir $OUTDIR
--- scripts/022_trinity_assembly.sh
+++ scripts/022_trinity_assembly.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=12
 #SBATCH --mem-per-cpu=4G
-#SBATCH --time=06:00:00
-#SBATCH --job-name=canu_assembly
+#SBATCH --time=1-00:00:00
+#SBATCH --job-name=trinity_assembly
 #SBATCH --mail-user=marco.visani@students.unibe.ch
 #SBATCH --mail-type=end,error
 #SBATCH --output=/data/users/mvisani/genome_assembly/scripts/output_%j.o
@@ -11,3 +11,12 @@
 #SBATCH --partition=pcourseassembly
 
 module load UHTS/Assembler/trinityrnaseq/2.5.1;
+
+FILES=/data/users/mvisani/genome_assembly/participant_2/RNAseq
+OUTDIR=/data/users/mvisani/genome_assembly/assembly/rna_trinity/
+
+Trinity --seqType fq \
+    --left $FILES/ERR754061_1.fastq.gz \
+    --right $FILES/ERR754061_2.fastq.gz \
+    --CPU 6 --max_memory 20G \
+    --output $OUTDIR 
